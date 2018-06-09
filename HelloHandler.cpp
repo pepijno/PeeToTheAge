1e76143a3da5693e1d3519a6811bf22520a6689f
bbosch-d <bbosch@deloitte.nl>
2018-06-10 01:27:14 +0200

    Lelijke empty file

---
+++ HelloHandler.cpp
@@ -0,0 +1,9 @@
+//~ #include "HelloHandler.h"
+
+
+//~ using namespace Pistache;
+
+
+//~ void HelloHandler::onRequest(const Http::Request& request, Http::ResponseWriter response) {
+	//~ response.send(Http::Code::Ok, "Hello World");
+//~ }
